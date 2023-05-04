3b406e44e3386d423bcce12d8710570d2564a2e0
VasiliyDR <vasilek96@bk.ru>
2023-05-04 14:31:21 +0300

    create files with consumers

---
+++ consumers/consumer_service_order_name.php
@@ -0,0 +1,8 @@
+<?php
+use php_deamon\classes\ConsumerServiceOrderNumberFrom1C;
+require_once __DIR__ . '/../classes/ConsumerServiceOrderNumberFrom1C.php';
+
+$consumer_service_order_number_from_1C = new ConsumerServiceOrderNumberFrom1C();
+$consumer_service_order_number_from_1C->run();
+
+?>
---
+++ consumers/consumer_service_spare_parts.php
@@ -0,0 +1,8 @@
+<?php
+use php_deamon\classes\ConsumerServiceSparePartsFrom1C;
+require_once __DIR__ . '/../classes/ConsumerServiceSparePartsFrom1C.php';
+
+$consumer_service_spare_parts_from_1C = new ConsumerServiceSparePartsFrom1C();
+$consumer_service_spare_parts_from_1C->run();
+
+?>
